693c81a7fcf275c9a22d08ae4a7b59163e9b97bb
churchrat <mihailkrustev08@gmail.com>
2024-03-21 23:34:08 +0200

    commit

---
+++ Paradonimi/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Paradonimi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            char[] duma = word.ToCharArray();
+            int mid = duma.Length / 2;
+            bool result = false;
+                for (int i = 0; i < mid; i++)
+                {
+                    if (duma[i] == duma[duma.Length - 1 - i])
+                    {
+                        result = true;
+                    }
+                    else
+                    {
+                        result = false;
+                    }
+                }
+            if (result)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+    }
+}
